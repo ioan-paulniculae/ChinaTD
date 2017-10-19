b57c86b4a17bc36084cd2e8ad043139d953bba60
Radu <piersicanu_radu2000@yahoo.com>
2017-10-19 21:57:40 +0300

    basic tower behaviour

---
+++ Assets/TowerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBehaviour : MonoBehaviour {
+
+    public float range;
+
+    CircleCollider2D collider;
+    public float fireRate;
+    public GameObject projectile;
+
+    List<GameObject> targetList = new List<GameObject>();
+    float cooldown;
+
+    // Use this for initialization
+    void Start () {
+        collider = gameObject.GetComponent<CircleCollider2D>();
+        collider.radius = range;
+        cooldown = 0;
+        targetList = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if( cooldown > 0)
+            cooldown -= Time.deltaTime;
+        if(targetList.Count > 0 )
+        {
+            cooldown = fireRate;
+            //shoot at targetQueue.Peek()
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        targetList.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        targetList.Remove(collision.gameObject);
+    }
+}
