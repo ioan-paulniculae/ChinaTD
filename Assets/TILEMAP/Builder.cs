fac3be415a5378c782f831f92a177b907bd15c6b
Radu <piersicanu_radu2000@yahoo.com>
2017-10-28 23:34:18 +0300

    map + add towers
    Am pus in scena map 3 tilemap-uri,
    unul e folosit pentru a verifica daca se poate construi turn, altul pentru environment si ultimul pentru turnuri,
    cand dai click pe un loc unde se poate construi, se instantiaza un turn si se pune un sprite in tilemap.

---
+++ Assets/TILEMAP/Builder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+
+public class Builder : MonoBehaviour
+{
+    private Tilemap tilemap;
+
+    //tilemap which contains sprites that can't be built on
+    public Tilemap buildableTilemap;
+
+    private Ray ray;
+    private Vector3Int mousePos;
+
+    public Tile towerSprite;
+    public Transform towerPrefab;
+
+    void Awake()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+        if (tilemap == null)
+        {
+            Debug.Log(gameObject.name + " does not have a Tilemap component");
+        }
+        
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if( Input.GetKeyDown("mouse 0"))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            mousePos = tilemap.WorldToCell(new Vector3(ray.origin.x, ray.origin.y, 0));
+
+
+            if( !buildableTilemap.HasTile(mousePos))
+            {
+                tilemap.SetTile(mousePos, towerSprite);
+                Instantiate(towerPrefab, tilemap.GetCellCenterWorld(mousePos), Quaternion.identity);
+            }
+        }
+    }
+}
