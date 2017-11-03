08d4817731defa03891302f836ac655e2cdc1d41
Radu <piersicanu_radu2000@yahoo.com>
2017-11-03 17:59:43 +0200

    Dragon

---
+++ Assets/Scripts/DragonBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonBehaviour : MonoBehaviour
+{
+
+    public float range;
+
+    public bool targetsGround = true;
+    public bool targetsAir = true;
+
+    public float fireRate;
+    public Transform projectile;
+
+    List<GameObject> targetList = new List<GameObject>();
+    float cooldown;
+
+    public float duration = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<CircleCollider2D>().radius = range;
+        cooldown = 0;
+        targetList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (targetList.Count > 0)
+            while (targetList[targetList.Count - 1] == null)
+            {
+                targetList.RemoveAt(targetList.Count - 1);
+            }
+
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else if (targetList.Count > 0)
+        {
+            cooldown = fireRate;
+            Transform proj = Instantiate(projectile, gameObject.transform.position, Quaternion.identity);
+            proj.GetComponent<ProjectileBehaviour>().target = targetList[0].transform;
+            //shoot at first member of targetlist   
+        }
+
+        if (duration > 0)
+        {
+            duration -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy" || other.tag == "AirEnemy")
+        {
+            targetList.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Enemy" || other.tag == "AirEnemy")
+        {
+            targetList.Remove(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/DragonDrag.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+
+public class DragonDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+    Transform startParent;
+
+    public Transform dragonPrefab;
+    public Transform range;
+
+    #region IBeginDragHandler implementation
+
+    private void Start()
+    {
+
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        startParent = transform.parent;
+        range.gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    #region IDragHandler implementation
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+
+        range.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        range.localScale = dragonPrefab.localScale * dragonPrefab.GetComponent<DragonBehaviour>().range * 2;
+    }
+
+    #endregion
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = startPosition;
+
+        Instantiate(dragonPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+        Debug.Log(Input.mousePosition);
+        GetComponent<Image>().color = Color.white;
+
+        range.gameObject.SetActive(false);
+    }
+
+    #endregion
+}
