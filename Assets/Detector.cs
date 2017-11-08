3569cc6bd9ac279dbeb30031cdaa854dc49a6027
Radu <piersicanu_radu2000@yahoo.com>
2017-11-08 21:02:33 +0200

    enemy shooter

---
+++ Assets/Detector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour {
+
+    private float minDistance = 100;
+
+    public GameObject shooter;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tower")
+        {
+            float distance = Vector3.Distance(transform.position, collision.gameObject.transform.position);
+            if (distance < minDistance)
+            {
+                shooter.GetComponent<EnemyShooterController>().target = collision.transform;
+                minDistance = distance;
+            }
+        }
+    }
+
+}
---
+++ Assets/EnemyShooterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooterController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public float rotationSpeed = 10.0f;
+    public Transform target;
+    public Transform baseTarget;
+    public float range = 5;
+    public float shootingRate = 1;
+    public Transform projectile;
+
+    private float cooldown;
+    private bool shooting = false;
+
+    private void moveTowardsTarget()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    private void lookAtTarget()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shooting = (Vector3.Distance(transform.position, target.transform.position) <= range);
+
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        } else if( shooting )
+		{
+			Transform proj = Instantiate(projectile, gameObject.transform.position, Quaternion.identity);
+			proj.GetComponent<ProjectileBehaviour>().target = target;
+			cooldown = shootingRate;
+		}
+
+        if (target == null)
+        {
+            target = baseTarget;
+            shooting = false;
+        }
+
+        if(!shooting)
+        {
+            moveTowardsTarget();
+            lookAtTarget();
+        }
+
+    }
+}
